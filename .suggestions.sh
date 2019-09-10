951779831acac3501ffdf6c34334a60dc4ae7d88
Paulo Sousa <pauloj10@gmail.com>
2019-09-11 00:08:09 +0200

    Add nice preview for long lines
    When the line is too long, truncate the middle, displaying the first
    and last 300 bytes.

--- .suggestions.sh
+++ .suggestions.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
-__suggestions_list_commands__() {
+__suggestions_preview__ () {
+  local cols input input_size
+  cols=$(tput cols)
+  input=$1
+  input_size=$(echo "$input" | wc -c)
+
+  if [ $input_size -gt 605 ]; then
+    echo $(echo $input | (head -c 300 && echo -e " \033[37;7m <...> \033[0m" && tail -c 300))
+  else
+    echo $input
+  fi
+}
+
+export -f __suggestions_preview__
+
+__suggestions_list_commands__ () {
   local command=$(
     history | \
     sort -n -r | \
     cut -c 8-  | \
-    fzf -q "$READLINE_LINE" --print-query --height ${FZF_TMUX_HEIGHT:-40%} | \
+    fzf -q "$READLINE_LINE" \
+      --print-query --height ${FZF_TMUX_HEIGHT:-40%} \
+      --preview-window down:4:wrap \
+      --preview '__suggestions_preview__ {}' | \
     tail -n 1)
 
   if [[ -z "$command" ]]
